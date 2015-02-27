583b12e5b58ee5a8dac4b94b88ca8110757ad414
ddv <derenok@gmail.com>
2015-02-27 09:53:22 +0500

    Удалил старый порект

--- arduino/PicavetFirmWare/Release/Regexp/Regexp.d
+++
@@ -1,5 +0,0 @@
-Regexp/Regexp.d Regexp/Regexp.o:  \
- E:/arduino/arduino-1.0.5-r2-windows/libraries/Regexp/Regexp.cpp \
-  E:/arduino/arduino-1.0.5-r2-windows/libraries/Regexp/Regexp.h
-
-E:/arduino/arduino-1.0.5-r2-windows/libraries/Regexp/Regexp.h:
